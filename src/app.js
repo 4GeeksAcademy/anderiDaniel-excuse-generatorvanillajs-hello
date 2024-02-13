42b92b005da7bebb2df4355fb8ff10a5fc47616b
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-13 19:35:03 +0000

    codigo app.js generador de excusas ralizado y vinculado al html. Falta darle algo de estilo y subirlo a 4Geeks

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
-const excuseGenerator
+const excuseGenerator = () => {
+  let excuse = [];
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-let texto = "hola, aca estoy yo";
+  excuse.push(who[Math.floor(Math.random() * 3 + 1)]);
+  excuse.push(" " + action[Math.floor(Math.random() * 3 + 1)]);
+  excuse.push(" " + what[Math.floor(Math.random() * 2 + 1)]);
+  excuse.push(" " + when[Math.floor(Math.random() * 4 + 1)]);
+  return excuse.join("");
+};
 
+let texto = excuseGenerator();
 document.getElementById("excuse").innerHTML = texto;
