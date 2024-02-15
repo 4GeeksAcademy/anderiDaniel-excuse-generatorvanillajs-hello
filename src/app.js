aadd7e235fb28f717e26828a0e24de48511ffa4d
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-15 01:48:24 +0000

    se escribio el codig dentro de window.onload

--- src/app.js
+++ src/app.js
@@ -1,20 +1,23 @@
-const excuseGenerator = () => {
-  let excuse = [];
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  excuse.push(who[Math.floor(Math.random() * 3 + 1)]);
-  excuse.push(" " + action[Math.floor(Math.random() * 3 + 1)]);
-  excuse.push(" " + what[Math.floor(Math.random() * 2 + 1)]);
-  excuse.push(" " + when[Math.floor(Math.random() * 4 + 1)]);
-  return excuse.join("");
+window.onload = function() {
+  const excuseGenerator = () => {
+    let excuse = [];
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    excuse.push(who[Math.floor(Math.random() * 3 + 1)]);
+    excuse.push(" " + action[Math.floor(Math.random() * 3 + 1)]);
+    excuse.push(" " + what[Math.floor(Math.random() * 2 + 1)]);
+    excuse.push(" " + when[Math.floor(Math.random() * 4 + 1)]);
+    return excuse.join("");
+  };
+  let texto = excuseGenerator();
+  document.getElementById("excuse").innerHTML = texto;
+  console.log(texto);
 };
-let texto = excuseGenerator();
-document.getElementById("excuse").innerHTML = texto;
