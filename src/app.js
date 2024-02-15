53753af41b2ba47c25c96281e002d49a19d16959
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-02-15 02:37:33 +0000

    se agrego parentesis a las variables.lenght -1

--- src/app.js
+++ src/app.js
@@ -11,13 +11,13 @@ window.onload = function() {
       "during my lunch",
       "while I was praying"
     ];
-    excuse.push(who[Math.floor(Math.random() * who.length + 1)]);
-    excuse.push(" " + action[Math.floor(Math.random() * action.length + 1)]);
-    excuse.push(" " + what[Math.floor(Math.random() * what.length + 1)]);
-    excuse.push(" " + when[Math.floor(Math.random() * when.length + 1)]);
+    excuse.push(who[Math.floor(Math.random() * (who.length - 1))]);
+    excuse.push(" " + action[Math.floor(Math.random() * (action.length - 1))]);
+    excuse.push(" " + what[Math.floor(Math.random() * (what.length - 1))]);
+    excuse.push(" " + when[Math.floor(Math.random() * (when.length - 1))]);
     return excuse.join("");
   };
   let texto = excuseGenerator();
   document.getElementById("excuse").innerHTML = texto;
-  console.log(texto);
+  // console.log(texto);
 };
